c9934c58521b1bc6875e81fd05e9f8f2624ed013
Alain <ads@sad.s>
2016-03-03 14:37:53 +0100

    la till main

--- mainProject/firstTest.java
+++ mainProject/firstTest.java
@@ -1,5 +1,9 @@
 package mainProject;
 
-public class firstTest {
+import com.sun.org.apache.xerces.internal.dom.PSVIDOMImplementationImpl;
 
+public class firstTest {
+	public static void main(String[] arg){
+		System.out.println("test");
+	}
 }
