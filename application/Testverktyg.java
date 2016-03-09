14c7fceed5a344517042ee8e6d27f67c6af4e8ca
Yanting Larsen <yanting.larsen@gmail.com>
2016-03-09 22:27:42 +0100

    Remove unused test files

--- application/Testverktyg.java
+++
@@ -1,8 +0,0 @@
-package application;
-
-public class Testverktyg {
-  
-  public static void main(String[]args){
-  System.out.println("hi guysf");
-  }
-}
--- mainProject/firstTest.java
+++
@@ -1,9 +0,0 @@
-package mainProject;
-
-import com.sun.org.apache.xerces.internal.dom.PSVIDOMImplementationImpl;
-
-public class firstTest {
-	public static void main(String[] arg){
-		System.out.println("test");
-	}
-}
