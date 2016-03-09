c2e36a0f5c6d5125a8fde872877bbea01222eb5b
michael sjögren <mikel-carl@hotmail.com>
2016-03-09 11:11:25 +0100

    Added files via upload
    GUI för admin samt events för knapparna

---
+++ AddNodes.java
@@ -0,0 +1,36 @@
+package application;
+
+import java.util.ArrayList;
+
+import javafx.scene.Group;
+import javafx.scene.control.CheckBox;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+public class AddNodes {
+
+	private ArrayList<String> textArray;
+
+	public VBox getCheckboxes(){
+		
+		VBox checkboxContainer = new VBox();
+		
+		for(int i = 0; i < 5; i++){
+		 CheckBox checkbox = new CheckBox();
+		 Group checkGroup = new Group();
+		 
+		 checkbox.setText("fasfsa");
+		 
+		 checkGroup.getChildren().add(checkbox);
+		 
+		 checkboxContainer.getChildren().add(checkGroup);
+		}
+		
+		return checkboxContainer;
+	}
+
+	public void setArray(ArrayList <String> textArray) {
+		this.textArray = textArray;
+		
+	}
+}
