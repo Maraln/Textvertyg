3864c807db12645203defaf71275a2e68d41fa0d
michael sjögren <mikel-carl@hotmail.com>
2016-03-10 09:45:33 +0100

    Added files via upload
    added close page button and a eventhandler for it in controller

--- AdminController.java
+++ AdminController.java
@@ -1,67 +1,67 @@
+package application;
+
 import java.util.ArrayList;
 import java.util.Optional;
-import javafx.application.Platform;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.scene.control.TextInputDialog;
 
 /** 
- * denna klass har till uppgift att koppa samman de andra klasserna som inehÃ¥ller logik 
+ * denna klass har till uppgift att koppa samman de andra klasserna som inehåller logik 
  * Controller klassen ska inte innefatta mycket logik
- * klassen innehÃ¥ller just nu 5 metoder 
+ * klassen innehåller just nu 5 metoder 
  * 
  * **/
 
 public class AdminController {
-    private AddNodes addNodes = new AddNodes();
-    
-    @FXML
-    private Button doneButton;
-    @FXML
-    private Button addTextfieldButton;
-    @FXML
-    private Button addCheckboxButton;
-    @FXML
-    private Button addTabButton;
-    @FXML
-    private Button statsButton;
-    @FXML
-    private TabPane tabPane;
-        
-    // event fÃ¶r meny-valen
-    @FXML
-    public void onClose(ActionEvent event) {
-        Platform.exit();
-    }
 	
-    // eventsen fÃ¶r knapparna
-    @FXML
-    public void onAddTextfield(ActionEvent event) {
-        System.out.println("dsada");
-    }
-
-    @FXML
-    public void onAddCheckbox(ActionEvent event) {
-        tabPane.getSelectionModel().getSelectedItem().setContent(addNodes.getCheckboxes());
-    }
-
-    @FXML
-    public void onAddTab(ActionEvent event) { 
-        Tab tab = new Tab("Page " + (tabPane.getTabs().size() + 1));
-        tabPane.getTabs().add(tab);
-    }
-
-    @FXML
-    public void onDone(ActionEvent event) {
-        System.out.println("knappen funkar");
-    }
-
-    @FXML
-    public void onShowStatistics(ActionEvent event) {
-        System.out.println("knappen funkar");
-    }
+	private AddNodes addNodes = new AddNodes();
+	
+	
+	@FXML
+	private Button doneButton;
+	@FXML
+	private Button addTextfieldButton;
+	@FXML
+	private Button addCheckboxButton;
+	@FXML
+	private Button addTabButton;
+	@FXML
+	private Button statsButton;
+	@FXML
+	private TabPane tabpane;
+	
+	// eventsen för knapparna
+	@FXML
+	public void onAddTextfield(ActionEvent event){
+		System.out.println("dsada");
+	}
+	
+	@FXML
+	public void onAddCheckbox(ActionEvent event){
+			tabpane.getSelectionModel().getSelectedItem().setContent(addNodes.getCheckboxes());
+	}
+	
+	@FXML
+	public void onAddTab(ActionEvent event){
+		System.out.println("knappen funkar");
+	}
+	
+	@FXML
+	public void onDone(ActionEvent event){
+		System.out.println("knappen funkar");
+	}
+	
+	@FXML
+	public void onShowStatistics(ActionEvent event){
+		System.out.println("knappen funkar");
+	}
+	
+	@FXML
+	public void onClosePage(){
+		
+	}
 }
